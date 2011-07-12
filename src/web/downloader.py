d02a3e1ce695ac82150ce5ce3efa3c313367303a
Daniel Jensen <daniel@daniel.home>
2011-07-12 15:17:47 -0400

    Fixed incomplete url in downloader.

--- src/web/downloader.py
+++ src/web/downloader.py
@@ -6,20 +6,22 @@ import urllib2
 def download(url, filetype='mp3'):
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page)
-    tags = soup.findAll('a', {'href' : re.compile('\.mp3')})
+    tags = soup.findAll('a', {'href' : re.compile('\.' + filetype)})
+    print tags
     for tag in tags:
         href = tag['href']
-        
-        file2dl = urllib2.urlopen(href)
-        
+
+        file2dl = urllib2.urlopen(url + href)
+
         filename = href.rsplit('/', 1)[-1]
         output = open(filename, 'wb')
         output.write(file2dl.read())
         output.close()
-        
+
 #        urllib2.urlopen(file)
-    
-    
+
+
 if __name__ == '__main__':
     url = "http://www.lds.org/library/display/0,4945,8009-1-4355-37,00.html"
-    download(url)
+    url = "http://www.physics.purdue.edu/academic_programs/courses/phys221/exam_archive/"
+    download(url, 'pdf')
